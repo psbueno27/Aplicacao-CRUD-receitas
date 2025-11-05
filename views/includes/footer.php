dae098db221369cb131801790ad3c0bc7d5b1f1a
psbueno27 <buenopedro07@gmail.com>
2025-11-05 00:29:35 -0300

    Add files via upload

---
+++ views/includes/footer.php
@@ -0,0 +1,7 @@
+        <hr>
+        <footer>
+            <p>&copy; <?php echo date("Y"); ?> CRUD PHP. Todos os direitos reservados.</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ views/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receitas da Omicron</title>
+    <link rel="stylesheet" href="/CRUD%20trabalho/public/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Lista de receitas da Omicron</h1>
+        <nav>
+            <a href="/CRUD%20trabalho/public/index.php?page=pratos">Pratos</a>
+            <a href="/CRUD%20trabalho/public/index.php?page=proteinas">Proteinas</a>
+        </nav>
+        <hr>
