8423e7dd940dbd60010fdb1fdcf0e03ba856c17a
psbueno27 <buenopedro07@gmail.com>
2025-11-05 22:10:21 -0300

    Update login.php

--- public/login.php
+++ public/login.php
@@ -1,4 +1,19 @@
 <?php
+/******************************************************************************
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Pedro Bueno (RA: 25181992-2)
+    
+Data: 4 de novembro de 2025
+Descritivo: Ponto para entrada do ambiente de login.              
+*******************************************************************************/
     session_start();
     include_once __DIR__ . '/../config/database.php'; // caminho pro seu database.php
 
@@ -70,3 +85,4 @@
 </body>
 
 </html>
+
