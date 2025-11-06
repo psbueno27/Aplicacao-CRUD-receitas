705b10ed773bb97b6babf906356d747f9997b341
psbueno27 <buenopedro07@gmail.com>
2025-11-05 22:04:33 -0300

    Update cadastrof.php

--- public/cadastrof.php
+++ public/cadastrof.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Cadastro</title>
-    <link rel="stylesheet" href="styles_log.css">
+    <link rel="stylesheet" href="/CRUD%20trabalho/public/css/styles_log.css">
 </head>
 <body>
     <div class="login">
@@ -51,4 +51,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     </div>    
         
 </body>
+
 </html>
